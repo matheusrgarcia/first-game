0f62a58644795023caa524a06da4eddbe13a2d21
Matheus Ribeiro Garcia <matheus_ribeiro.g@hotmail.com>
2020-03-15 22:58:52 -0300

    Game commit

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public float groundCheckRadious;
+    public LayerMask whatIsGround;
+    private bool onGround;
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        // Current Velocity
+        rb.velocity = new Vector2(2, rb.velocity.y);
+
+        onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadious, whatIsGround);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            print("On ground?" + onGround);
+        }
+
+        // Why I can't get my onGround to work as expected? :/ not a code problem though
+        if(Input.GetMouseButtonDown(0) && onGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 4);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# first-game
+# First-game
+
+- Meu primeiro jogo usando o Unity :)
+
+## To do List.
+
+- Fazer o personagem checar o ground
+
+* Fazer o personagem não girar ao bater nas estruturas
+
+- Adicionar algo para destruir as caixas?.
+
+* Criar um mapa infinito.
